cb1b0030c22d98fc7a9aad11a395fbb50a2b3f26
nhutnhut1 <nhutp49@gmail.com>
2022-06-27 20:41:20 +0700

    edit Search and Graph

--- src/components/AccountItem/AccountItem.tsx
+++ src/components/AccountItem/AccountItem.tsx
@@ -10,8 +10,8 @@ const AccountItem: React.FC<{}> = () => {
             <NotificationIcon className="app__account-item__notification-icon" size={24} />
             <Link className="app__account-item__account-avatar" to={'/dashboard'}>
                 <img
-                    src="https://scontent.fsgn2-1.fna.fbcdn.net/v/t39.30808-6/251374287_946849012708805_4517610628801416846_n.jpg?_nc_cat=105&ccb=1-7&_nc_sid=09cbfe&_nc_ohc=mfu7HJx8WTUAX_W6waa&_nc_ht=scontent.fsgn2-1.fna&oh=00_AT8tPJqbHn5lSUbHIdN4O5E858kKHg6B-JzlLH0nX534zA&oe=62B969FD"
-                    alt="money"
+                    src="https://scontent.fsgn2-1.fna.fbcdn.net/v/t39.30808-6/251374287_946849012708805_4517610628801416846_n.jpg?_nc_cat=105&ccb=1-7&_nc_sid=09cbfe&_nc_ohc=VTEMJkIdurYAX-xRszK&_nc_ht=scontent.fsgn2-1.fna&oh=00_AT92KhV3AkFD1TjH7X8Qi9uW1h5o_VWW701fRMHgkx6LlA&oe=62BF58BD"
+                    alt="Min Nhut"
                 />
             </Link>
         </div>
